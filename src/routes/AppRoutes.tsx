e1a23b3bd3b209955fd901f11f152f9d2228bbeb
chetan chandel <chetanchandel3@gmail.com>
2024-02-07 11:35:33 +0530

    home page

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,13 +2,14 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import { routes } from "./routes";
 
 import NotFound from "@/pages/NotFound";
+import Home from "@/pages/Home";
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path={"/"} element={<Navigate replace to={routes.home.path} />} />
 
-      <Route path={routes.home.path} element={<>home</>} />
+      <Route path={routes.home.path} element={<Home />} />
       <Route path={routes.settings.path} element={<>settings</>} />
 
       <Route element={<NotFound />} path="*" />
